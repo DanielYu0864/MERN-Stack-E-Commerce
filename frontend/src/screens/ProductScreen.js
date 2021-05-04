60a2edba7bed31a8ec9475aea3d80217ddfdab2e
Daniel.Yu <adam741963@gmail.com>
2021-05-04 14:53:47 -0700

    SEction 6: Adding The Shopping Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,36 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import { listProductDetails } from '../actions/productActions'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { listProductDetails } from '../actions/productActions';
 
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
 
-const ProductScreen = ({ match }) => {
-  const dispatch = useDispatch()
-
-  const productDetails = useSelector(state => state.productDetails)
-  const { loading, error, product } = productDetails
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    dispatch(listProductDetails(match.params.id))
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
-
-  }, [dispatch, match])
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`); // redirect
+  };
 
   return (
     <>
-     <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
 
-     {
-       loading ? <Loader/>
-       : error ? <Message variant='danger'>{error}</Message>
-       : (
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <Row>
           <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid/> {/* fluid: keep image inside the compoment */}
+            <Image src={product.image} alt={product.name} fluid />{' '}
+            {/* fluid: keep image inside the compoment */}
           </Col>
           <Col md={3}>
             <ListGroup variant='flush'>
@@ -38,11 +52,12 @@ const ProductScreen = ({ match }) => {
                 <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Rating value={product.rating} text={`${product.numReviews} review`}/>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} review`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item>
-                Price: ${product.price}
-              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
               <ListGroup.Item>
                 Description: {product.description}
               </ListGroup.Item>
@@ -54,9 +69,7 @@ const ProductScreen = ({ match }) => {
               <ListGroup variant='flush'>
                 <ListGroup.Item>
                   <Row>
-                    <Col>
-                      Price:
-                    </Col>
+                    <Col>Price:</Col>
                     <Col>
                       <strong>${product.price}</strong>
                     </Col>
@@ -65,30 +78,51 @@ const ProductScreen = ({ match }) => {
 
                 <ListGroup.Item>
                   <Row>
-                    <Col>
-                      Status:
-                    </Col>
+                    <Col>Status:</Col>
                     <Col>
                       {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
-                  <Button className='btn-block p-1' type='button' disabled={product.countInStock === 0}>
+                  <Button
+                    onClick={addToCartHandler}
+                    className='btn-block p-1'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
                     Add to Cart
                   </Button>
                 </ListGroup.Item>
-
               </ListGroup>
             </Card>
           </Col>
-
         </Row>
-      )
-    }
-
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
