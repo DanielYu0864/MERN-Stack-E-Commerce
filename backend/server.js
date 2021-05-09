880ed104f335842af4d8206f24feda93a14046d4
Daniel.Yu <adam741963@gmail.com>
2021-05-08 20:30:23 -0700

    Section 7: Back End User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,36 @@
-import express from 'express'
-import dotenv  from 'dotenv'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js' //! file has to have filename.js in backend
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js'; //! file has to have filename.js in backend
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+  res.send('API is running...');
+});
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(notFound)
+app.use(notFound);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
     `Sever running in ${process.env.NODE_ENV} on port: ${PORT}`.yellow.bold
   )
-)
+);
